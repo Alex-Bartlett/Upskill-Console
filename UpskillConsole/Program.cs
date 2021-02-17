87ddd7a27d1782871fcfc9e66a4a3efde6b6184c
Alex Bartlett <ab1425@canterbury.ac.uk>
2021-02-17 13:39:04 +0000

    Added VS files

---
+++ UpskillConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace UpskillConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
